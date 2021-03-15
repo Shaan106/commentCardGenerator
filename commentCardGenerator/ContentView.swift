f9f2e56cef0f0769e7f67be677a7feaf3686b0f7
Aasmaan Yadav <yadav.a@etoncollege.org.uk>
2021-03-15 15:24:55 +0000

    Initial Commit

---
+++ commentCardGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  commentCardGenerator
+//
+//  Created by Yadav, Aasmaan (SPH) on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ commentCardGenerator/commentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  commentCardGeneratorApp.swift
+//  commentCardGenerator
+//
+//  Created by Yadav, Aasmaan (SPH) on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct commentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
