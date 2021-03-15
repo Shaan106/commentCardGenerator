442f244f93fc339ce8266c2f8f74fefd887f22c8
Aasmaan Yadav <yadav.a@etoncollege.org.uk>
2021-03-15 15:51:16 +0000

    made start

--- commentCardGenerator/ContentView.swift
+++ commentCardGenerator/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var subjects = ["Physics","Biology","Chemistry","Computer Science", "Greek"]
+    @State private var selectedSubject = "Computer Science"
+    
+    @State private var happinessInSubject: Double = 0
+    @State private var effortInSubject: Double = 0
+    @State private var progressInSubject: Double = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            VStack {
+                Picker("Please choose a subject", selection: $selectedSubject) {
+                    ForEach(subjects, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Text("You have selected: \(selectedSubject)")
+
+                Slider(value: $happinessInSubject, in: 0...10)
+                    .padding()
+                Text("Happiness is \(happinessInSubject, specifier: "%.1f")")
+                
+                Slider(value: $effortInSubject, in: 0...10)
+                    .padding()
+                Text("Effort is \(effortInSubject, specifier: "%.1f")")
+                
+                Slider(value: $progressInSubject, in: 0...10)
+                    .padding()
+                Text("Progress: \(progressInSubject, specifier: "%.1f")")
+            }
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Comment Card Writer")
+                        .font(.title)
+                        .bold()
+                }
+            }
+            .font(.headline)
+        }
     }
 }
 
